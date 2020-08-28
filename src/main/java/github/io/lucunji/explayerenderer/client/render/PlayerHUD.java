9b9c0407d84769a75fcaf8b0c601f97f9de02eb6
LucunJi <1063464500@qq.com>
2020-08-27 19:10:45 -0500

    simplify code, still cannot fix bugs

--- src/main/java/github/io/lucunji/explayerenderer/client/render/PlayerHUD.java
+++ src/main/java/github/io/lucunji/explayerenderer/client/render/PlayerHUD.java
@@ -9,24 +9,13 @@ import net.minecraft.client.render.entity.EntityRenderDispatcher;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.client.util.math.Vector3f;
 import net.minecraft.entity.Entity;
-import net.minecraft.entity.data.TrackedData;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.util.math.MathHelper;
 import net.minecraft.util.math.Quaternion;
 
-import java.lang.reflect.Field;
-import java.util.Arrays;
-
 public class PlayerHUD extends DrawableHelper {
     private static final MinecraftClient client = MinecraftClient.getInstance();
-    private static final Field flagsField;
 
-    static {
-        flagsField = Arrays.asList(Entity.class.getDeclaredFields()).stream().filter(f -> f.getType() == TrackedData.class && f.getGenericType().getTypeName().contains("<java.lang.Byte>")).findFirst().orElse(null);
-        flagsField.setAccessible(true);
-    }
-
-    // TODO: lightDegree
     public void render(int ticks) {
         PlayerEntity player;
         if (Configs.SPECTATOR_AUTO_SWITCH.getBooleanValue()) {
@@ -48,7 +37,6 @@ public class PlayerHUD extends DrawableHelper {
         if (player.isFallFlying()) posY += Configs.ELYTRA_OFFSET_Y.getDoubleValue();
         double size = Configs.SIZE.getDoubleValue() * scaledHeight;
         boolean mirror = Configs.MIRROR.getBooleanValue();
-//        double lightDegree = Configs.LIGHT_DEGREE.getDoubleValue();
 
         RenderSystem.pushMatrix();
         {
@@ -73,15 +61,7 @@ public class PlayerHUD extends DrawableHelper {
             float headYaw = player.headYaw;
             float handSwingProgress = player.handSwingProgress;
             int hurtTime = player.hurtTime;
-            int fireTicks = player.getFireTicks();
-            byte flags = 0;
-            try {
-                 flags = player.getDataTracker().get((TrackedData<Byte>) flagsField.get(player));
-            } catch (IllegalAccessException e) {
-                e.printStackTrace();
-            }
             EntityRenderDispatcher entityRenderDispatcher = MinecraftClient.getInstance().getEntityRenderManager();
-            boolean renderHitbox = entityRenderDispatcher.shouldRenderHitboxes();
 
             player.bodyYaw = 180 - (float) MathHelper.clamp(player.bodyYaw, Configs.BODY_YAW_MIN.getDoubleValue(), Configs.BODY_YAW_MAX.getDoubleValue());
             player.headYaw = 180 - (float) MathHelper.clamp(player.headYaw, Configs.HEAD_YAW_MIN.getDoubleValue(), Configs.HEAD_YAW_MAX.getDoubleValue());
@@ -97,22 +77,11 @@ public class PlayerHUD extends DrawableHelper {
                 player.hurtTime = 0;
             }
 
-            player.setFireTicks(0);
-            try {
-                player.getDataTracker().set((TrackedData<Byte>) flagsField.get(player), (byte) (flags & -2)); // -2 = 0b1111...11110
-            } catch (IllegalAccessException e) {
-                e.printStackTrace();
-            }
-
             quaternion2.conjugate();
-            entityRenderDispatcher.setRenderHitboxes(false);
             entityRenderDispatcher.setRotation(quaternion2);
-            entityRenderDispatcher.setRenderShadows(false);
             VertexConsumerProvider.Immediate immediate = MinecraftClient.getInstance().getBufferBuilders().getEntityVertexConsumers();
-            entityRenderDispatcher.render(player, 0.0D, 0.0D, 0.0D, 0.0F, 1.0F, matrixStack, immediate, 15728880);
+            entityRenderDispatcher.getRenderer(player).render(player, yaw, 1, matrixStack, immediate, 15728880);
             immediate.draw();
-            entityRenderDispatcher.setRenderShadows(true);
-            entityRenderDispatcher.setRenderHitboxes(renderHitbox);
 
             player.bodyYaw = bodyYaw;
             player.yaw = yaw;
@@ -121,12 +90,6 @@ public class PlayerHUD extends DrawableHelper {
             player.headYaw = headYaw;
             player.handSwingProgress = handSwingProgress;
             player.hurtTime = hurtTime;
-            player.setFireTicks(fireTicks);
-            try {
-                player.getDataTracker().set((TrackedData<Byte>) flagsField.get(player), flags);
-            } catch (IllegalAccessException e) {
-                e.printStackTrace();
-            }
         }
         RenderSystem.popMatrix();
     }
