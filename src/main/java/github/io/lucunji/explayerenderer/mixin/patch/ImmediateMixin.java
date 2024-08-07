d7c61efa9a86060a5120c961860f522c89834d82
lucunji <lucunjif@gmail.com>
2024-08-07 14:02:40 -0400

    fix mixin that has no effect

--- src/main/java/github/io/lucunji/explayerenderer/mixin/patch/ImmediateMixin.java
+++ src/main/java/github/io/lucunji/explayerenderer/mixin/patch/ImmediateMixin.java
@@ -1,13 +1,15 @@
 package github.io.lucunji.explayerenderer.mixin.patch;
 
-import com.llamalad7.mixinextras.injector.wrapmethod.WrapMethod;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.mojang.blaze3d.systems.RenderSystem;
 import github.io.lucunji.explayerenderer.mixininterface.ImmediateMixinInterface;
+import net.minecraft.client.render.BufferBuilder;
 import net.minecraft.client.render.RenderLayer;
 import net.minecraft.client.render.VertexConsumerProvider;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
 
 @Mixin(VertexConsumerProvider.Immediate.class)
 public abstract class ImmediateMixin implements ImmediateMixinInterface {
@@ -18,14 +20,15 @@ public abstract class ImmediateMixin implements ImmediateMixinInterface {
     @Override
     public void extraPlayerRenderer$setForceDisableCulling(boolean disableCulling) { this.forceDisableCulling = disableCulling; }
 
-    @WrapMethod(method = "draw(Lnet/minecraft/client/render/RenderLayer;)V")
-    void forceDisableCulling(RenderLayer layer, Operation<Void> original) {
+    @WrapOperation(method = "draw(Lnet/minecraft/client/render/RenderLayer;)V",
+            at = @At(value = "INVOKE", target = "Lnet/minecraft/client/render/VertexConsumerProvider$Immediate;draw(Lnet/minecraft/client/render/RenderLayer;Lnet/minecraft/client/render/BufferBuilder;)V"))
+    void disableCulling(VertexConsumerProvider.Immediate instance, RenderLayer layer, BufferBuilder builder, Operation<Void> original) {
         if (this.forceDisableCulling) {
             RenderSystem.disableCull();
-            original.call(layer);
+            original.call(instance, layer, builder);
             RenderSystem.enableCull();
         } else {
-            original.call(layer);
+            original.call(instance, layer, builder);
         }
     }
 }
